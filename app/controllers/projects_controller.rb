c10bb6bf37685fa3d3eb27bb0d1e2b4a586aee63
Shailendra <shailendra.tout@gmail.com>
2022-12-05 12:50:21 +0530

    d

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,7 +8,7 @@ class ProjectsController < ApplicationController
   def index
     @projects=Project.all
   end
- 
+ #
   def create
     puts params[:project][:logo_image]
     @project=Project.new(project_params)
